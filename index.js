114872b00bca9c043a907adb214856fb26cbf817
McCambley <jakemccamb16@gmail.com>
2025-01-09 01:16:03 -0500

    Remove replacement of loading text from ski report display

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const url = "https://jxc-updates.onrender.com";
 async function getSkiReport() {
   const reportEl = document.querySelector(".main__text");
-  reportEl.textContent = "Loading Report...";
   try {
     const response = await fetch(`${url}/ski-report`);
     const { data } = await response.json();

966b9fcf15b2359293355698d2194f8f7b59a291
McCambley <jakemccamb16@gmail.com>
2025-01-20 20:48:04 -0500

    Create mock loading bar

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const url = "https://jxc-updates.onrender.com";
 // const url = "http://localhost:3000";
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function getSkiReport() {
   const reportEl = document.querySelector(".main__text");
   const body = document.querySelector("body");
+  const loadingWrapper = document.querySelector(".main__loading-wrapper");
+  const loadingBar = document.querySelector(".main__loading-bar");
   try {
     const response = await fetch(`${url}/ski-report`);
     const { data } = await response.json();
     const paragraphs = data.split("\n\n");
-    reportEl.innerHTML = "";
+    loadingBar.classList.add("no-animation");
+    await sleep(300);
     body.style.padding = "1rem";
+    reportEl.innerHTML = "";
     paragraphs.forEach((p) => {
       const el = document.createElement("p");
       el.textContent = p;
       reportEl.appendChild(el);
+      loadingWrapper.remove();
     });
   } catch (error) {
+    loadingWrapper.remove();
     reportEl.innerHTML = "";
     const el = document.createElement("p");
     el.textContent =
